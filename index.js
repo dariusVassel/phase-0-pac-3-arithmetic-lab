52964bba734dc7d693a29cf76b5b5ac11707377b
+ <+>
2021-06-27 11:17:42 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(){
+    return x+y;
+}
+
+function subtract(){
+    return x-y;
+}
+
+function multiply(){
+    return x*y;
+}
+
+function divide(){
+    return x/y;
+}
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function increment(n){
+    return (n += 1);
+}
+
+function decrement(n){
+    return (n -= 1) ;
+} 
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
